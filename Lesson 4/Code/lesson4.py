6150b0d9a55e2e39da7620219cbdbacb6931996a
Viet <Viet>
2022-05-07 21:33:19 +0700

    add lesson 4 + labs

---
+++ Lesson 4/Code/lesson4.py
@@ -0,0 +1,9 @@
+# viet chuong trinh lien tuc yeu cau nguoi dung nhap 'q'
+# neu ko chuong trinh se tiep tuc chay...
+ch = input()
+
+while ch != 'q':
+    print("ohnoooooooooo shibaaaaaa....")
+    ch = input()
+print("helu shibe")
+

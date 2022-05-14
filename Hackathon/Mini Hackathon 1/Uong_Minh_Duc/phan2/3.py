f484c68a7242549df31c02249592541901588736
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-05-14 21:29:19 +0700

    Create 3.py

---
+++ Hackathon/Mini Hackathon 1/Uong_Minh_Duc/phan2/3.py
@@ -0,0 +1,7 @@
+number=int(input("number:"))
+if number==int:
+     for i in range(0, number+1):
+         if number%2!=0:
+             print(i,number="") 
+else:
+  print("invalid input, try again")

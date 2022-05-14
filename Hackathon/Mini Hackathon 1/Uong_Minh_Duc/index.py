59d33e21baa58340d91d90ff3e88094618c9a748
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-05-14 21:22:38 +0700

    Create index.py

---
+++ Hackathon/Mini Hackathon 1/Uong_Minh_Duc/index.py
@@ -0,0 +1,86 @@
+# EX1
+#p1
+# first=input("First name:")
+# last=input("Last name:")
+# print("Your full name is:" ,first , last)
+
+#p2
+# text=input("text that need to be caps:")
+# print(text.upper())
+
+#p3
+# binhphuong=float(input("number that need to be squared up:"))
+# print(pow(binhphuong,2))
+
+#p4
+# import turtle
+# t=turtle.Turtle()
+# radius=float(input("type the radius:"))
+# t.circle(radius)
+
+# EX2
+#p1
+# for i in range(3, 13):
+#     print(i)
+
+#p2
+# number=int(input("number:"))
+# if number==int:
+#     for i in range(0, number+1):
+#         print(i)
+# else:
+#     print("invalid input, try again")
+
+#p3
+# number=int(input("number:"))
+# if number==int:
+#     for i in range(0, number+1):
+#         if number%2!=0:
+#             print(i,number="") 
+# else:
+#     print("invalid input, try again")
+
+#p4
+# import turtle
+# t=turtle.Turtle()
+# side=int(input("side number:"))
+# for _ in range(50):
+#     turtle.forward(50)
+#     turtle.right(360/side)
+
+# EX3
+#p1
+# number=int(input("number: "))
+# if number>13:
+#     print("so lon hon 13")
+# else:
+#     print("so nho hon 13")
+
+#p2
+# number=int(input("number: "))
+# if number%2==0:
+#     print("so chan")
+# else:
+#     print("ko phai so chan")
+
+#p3
+
+# em ko biet lam anh oi cuu em huhu
+
+# EX4
+#p1+2+3?
+# print("REGISTRATION SIMULATOR")
+# username=(input("your username pls:"))
+# print("your username is:",username)
+# password=(input("your pass pls:"))
+# checkpass=(input("repeat your pass pls:"))
+# while True:
+#     password=(input("your pass pls:"))
+#     checkpass=(input("repeat your pass pls:")) 
+#     if password==checkpass:
+#         print("your pass is:" ,password)
+#         break
+#     else:
+#         print("pasword not match, try again")
+# email=(input("your mail pls:"))
+# print("Registered successfully")

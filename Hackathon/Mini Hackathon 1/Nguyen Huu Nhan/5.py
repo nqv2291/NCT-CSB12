5ca19fe69b97647e44f0c7b020f1700bc68ff02a
NhanHu <104263824+NhanHu@users.noreply.github.com>
2022-05-15 07:43:46 +0700

    Update 5.py

--- Hackathon/Mini Hackathon 1/Nguyen Huu Nhan/5.py
+++ Hackathon/Mini Hackathon 1/Nguyen Huu Nhan/5.py
@@ -18,3 +18,46 @@ if Answer_input == 1 and Answer == Randomize_interger1 + Randomize_interger2:
     print ("your score is {score}")
 elif Answer_input == 0 and Answer != Randomize_interger1 + Randomize_interger2:
     print("Nice")
+  
+  
+  
+  
+ #Moi xong:)) 9:59 14/05/2022
+  import random
+print ("give correct answer")
+Randomize_sign = random.randint (0,3)
+Randomize_interger1 = random.randint (1,30)
+Randomize_interger2 = random.randint (1,30)
+score = 0
+Loop = 0
+while Loop == 0:
+    Randomize_answer = random.randint (-2,2)
+    if Randomize_sign == 0:
+        Answer = Randomize_interger1 + Randomize_interger2 
+        Answer = Answer  + Randomize_answer 
+        print (f"{Randomize_interger1} +  {Randomize_interger2} = {Answer}")
+    elif Randomize_sign == 1:
+        Answer = Randomize_interger1 - Randomize_interger2 
+        Answer = Answer + Randomize_answer 
+        print (f"{Randomize_interger1} - {Randomize_interger2} = {Answer}")
+    elif Randomize_sign == 2:
+        Answer = Randomize_interger1 * Randomize_interger2 
+        Answer = Answer + Randomize_answer 
+        print (f"{Randomize_interger1} * {Randomize_interger2} = {Answer}")
+    Answer_input = input("1 for True, 0 for False:")
+    if Answer_input == str(0):
+        if Answer == Randomize_interger1 + Randomize_interger2 or Answer == Randomize_interger1 - Randomize_interger2 or Answer == Randomize_interger1 * Randomize_interger2:
+            print ("Lost")
+            Loop = Loop + 1
+        else:
+            score = score + 1
+            print (f"ur score is {score}")
+    elif Answer_input == str(1):
+        if Answer == Randomize_interger1 + Randomize_interger2 or Answer == Randomize_interger1 - Randomize_interger2 or Answer == Randomize_interger1 * Randomize_interger2:
+            score = score + 1
+            print (f"ur score is {score}")
+        else:
+            print ("Lost")
+            Loop = Loop + 1
+    
+print (f"Your total score is {score}")

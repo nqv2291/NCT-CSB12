0d29b862b94759b0fcc55f39a24de777a90e6844
Viet <Viet>
2022-05-21 20:14:11 +0700

    add lesson 6

--- Labs/Lab04/Students/Bui_Nhat_Minh/bai5.py
+++ Labs/Lab04/Students/Bui_Nhat_Minh/bai5.py
@@ -5,8 +5,8 @@ while d > 0:
     a += 1
     b = str(a)
     c = 0
-    for i in range(len(b)):
-        c += int(b[i])
+    for i in b:
+        c += int(i)
     if c == 9:
         print(a, end=' ')
         d -= 1

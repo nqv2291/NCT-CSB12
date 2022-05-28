b5b5e3c6ba4158d7645ec8a9c9c0b5171c32c60e
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-05-28 21:36:00 +0700

    Create p2.py

---
+++ Hackathon/Mini_Hackathon2/Uong_Minh_Duc/p2.py
@@ -0,0 +1,11 @@
+#p1
+arr=["blue","red","teal","green"]
+print("color list:",arr)
+input=int(input("Input position:" ))
+print("color position at",input,":",arr[input-1])
+#p2
+arr=["blue","red","teal","green"]
+print("color list:",arr)
+input=int(input("Input position need to delete:" ))
+print("color position at",input,":",arr[input-1])
+#p3

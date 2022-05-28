c8dda562985a6b1bd7131593bed9bbf7caa51661
Nguyễn Việt Anh <70436782+Vanh1902@users.noreply.github.com>
2022-05-28 22:25:59 +0700

    Nguyen Viet Anh - hackathon2
    update code

--- Hackathon/Mini_Hackathon2/Nguyen_Viet_Anh/phan_2/bai_2.py
+++ Hackathon/Mini_Hackathon2/Nguyen_Viet_Anh/phan_2/bai_2.py
@@ -1,18 +1,15 @@
 lst = ["Blue", "Green", "Purple", "Gray"]
-nLST = [""]
+nLST = []
 print("Color list: ")
 for i in lst:
     print(i,end=" ")
 print("")
 
-print("""Delete using word or number:
-    Press 1 to delete using word
-    Press 2 to delete using number""")
 
-b = input()
-if b == 1:
-    c = input("Item to delete: ")
-    nLST = lst.remove(c)
-else:
-    c = int(input("Item to delete: "))
-    nLST = lst.pop(c + 1)
+# c = input("Item to delete: ")
+# lst.remove(c)
+
+# d = int(input("Item to delete: "))
+# lst.pop(d - 1)
+    
+print(lst)

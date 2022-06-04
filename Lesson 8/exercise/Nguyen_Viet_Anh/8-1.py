da6a704459f04118dbc1aa8175d5e4627115e9ac
Nguyễn Việt Anh <70436782+Vanh1902@users.noreply.github.com>
2022-06-04 20:54:24 +0700

    Nguyen Viet Anh - exercise

---
+++ Lesson 8/exercise/Nguyen_Viet_Anh/8-1.py
@@ -0,0 +1,3 @@
+def display_message():
+    print("I had learning about function")
+display_message()
---
+++ Lesson 8/exercise/Nguyen_Viet_Anh/8-3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    print(f"this T-shirt has size {size} and a message: {message} ")
+
+#cach 1
+make_shirt("M","Viet Nam")
+#cach 2
+make_shirt(size = "M", message="Viet Nam")

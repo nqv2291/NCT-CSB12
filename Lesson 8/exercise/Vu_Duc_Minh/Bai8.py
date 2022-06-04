25b8f8b86750981a65ca1b448b454228df56e0e9
Snavy05 <46926922+Snavy05@users.noreply.github.com>
2022-06-04 20:54:50 +0700

    exercises

---
+++ Lesson 8/exercise/Vu_Duc_Minh/Bai8.py
@@ -0,0 +1,34 @@
+# 1
+# def display_message():
+#     print("I'm learning about functions!")
+
+# display_message()
+
+#2
+# def fav_book(title):
+#     print("One of my favorite books is", title)
+
+# fav_book("Alice in Wonderland.")
+
+# 3:
+def make_shirt(s,msg):
+    print("The size of the shirt is:",s)
+    print("The message to be printed on the shirt:",msg)
+
+size = input()
+msg = input()
+make_shirt(42, "'This is a shirt'")
+make_shirt(size, msg)
+
+# 4:
+# def lar_shirts(lar, med, any):
+#     print("The message for",lar,"and",med,"shirts are: I love Python!")
+#     print("")????
+
+# 5:
+def des_city(city, country):
+    print(city,"is the capital of",country)
+
+des_city("Reykjavik", "Iceland")
+des_city("London", "England")
+des_city("Washington, D.C", "US")

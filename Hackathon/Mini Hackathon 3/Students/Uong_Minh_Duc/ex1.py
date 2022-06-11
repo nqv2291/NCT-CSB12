73b510ebd41f61df5c4e4fb0017e5555b7b49f41
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-06-11 21:33:15 +0700

    Create ex1.py

---
+++ Hackathon/Mini Hackathon 3/Students/Uong_Minh_Duc/ex1.py
@@ -0,0 +1,28 @@
+#b1
+# check=int(input("Nhap so:")) 
+# def checknum(check):
+#     if check%2==0: 
+#         print("so chan")
+#     else: 
+#         print("ko phai so chan")
+#b2
+# radius=int(input("Nhap so:")) 
+# def cal_area(radius):
+#     area=3.13*pow(radius,2)
+#     return area
+# print("area:2",cal_area(radius))
+#b3
+# text=str(input("Nhap day ki tu:")) 
+# def reverse_str(text):
+#     return text[::-1]
+# print("reverse:",reverse_str(text))
+#b4
+# text=str(input("Nhap day ki tu:"))
+# def is_palindrome(text):
+#     text[::-1]
+#     check=text[:]
+#     if check==text: 
+#         return "palindrome"
+#     else: 
+#         return "ko phai palindrome"
+# print(is_palindrome(text))

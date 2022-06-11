4caa8a71facb104cbdba350768c477749a413891
Duc <51939597+minhduc5009sg@users.noreply.github.com>
2022-06-11 21:33:27 +0700

    Create ex2.py

---
+++ Hackathon/Mini Hackathon 3/Students/Uong_Minh_Duc/ex2.py
@@ -0,0 +1,32 @@
+#b1
+# num=int(input("Nhap so:"))
+# def giai_thua(num):
+#     if num==0:
+#         return 1
+#     else:
+#         return num*giai_thua(num-1)
+# print(giai_thua(num))
+#b2
+# list=[5,1,8,92,-1,30]
+# startnum=list[0]
+# print("un-sorted list:",list)
+# def sort(list):
+#     while sort:
+#         for i in list:
+#             if i<startnum:
+#                 i=startnum
+#         list.append(startnum)
+#         list.remove(startnum) 
+# print("sorted list:",sort(list))
+#b3
+# n=int(input("Nhap so:"))
+# dem=0
+# start1,start2=1,1
+# def print_fibo(n):
+#     while dem<n:
+#         print(start1)
+#         term=start1+start2
+#         start1=start2
+#         term=start2
+#         dem+=1
+# print(print_fibo(n))
